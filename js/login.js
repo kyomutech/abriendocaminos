e82fc2e1ef4ef370c9d7e4ef23920ea41bf15e13
rrojasj <roberto.rojas@kyomutech.com>
2024-10-12 08:47:31 -0600

    index.html & login.html

---
+++ js/login.js
@@ -0,0 +1,14 @@
+`use strict`
+
+/**
+ * @description Obtain the element values from DOM
+ */
+var inputEmail = document.getElementById('login-email').value;
+var inputPswd = document.getElementById('login-pswd').value; 
+
+
+
+/**
+ * @name validateLoginInputs
+ * @description Validates if any field has the required input text
+ */
